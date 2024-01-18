b39a5aebbe799864f966ba7e3124c8e773f2e5e5
Celio <celio1878@hotmail.com>
2024-01-17 21:12:21 -0300

    feat: Remove unused files

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-/** @type {{experimental: {instrumentationHook: boolean, webVitalsAttribution: (string)[]}, i18n: {locales: string[]}}} */
 const nextConfig = {
-  i18n: {
-    locales: ["en-US", "pt-BR", "de-DE", "fr", "es"],
-    defaultLocale: "en-US",
-    localeDetection: false,
-  },
   experimental: {
     webVitalsAttribution: ["CLS", "LCP"],
-    instrumentationHook: true,
   },
 };
 
