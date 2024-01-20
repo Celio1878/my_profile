c319325b1b0e095dd7f0b537d6ca61ee9b0ce37e
Celio <celio1878@hotmail.com>
2024-01-19 21:29:39 -0300

    feat: add links

--- src/components/shell/footer.tsx
+++ src/components/shell/footer.tsx
@@ -1,41 +1,27 @@
 import { FC } from "react";
-import Link from "next/link";
 import { NpmIcon } from "@/components/icons/npmIcon";
 import { GithubIcon } from "@/components/icons/githubIcon";
 import { LinkedInIcon } from "@/components/icons/linkedInIcon";
+import { GithubLink } from "@/components/links/githubLink";
+import { NpmPackagesLink } from "@/components/links/npmPackagesLink";
+import { LinkedinLink } from "@/components/links/linkedinLink";
+import { VercelLink } from "@/components/links/vercelLink";
 
 export const Footer: FC = () => (
   <footer className="flex flex-col w-full h-28 justify-between items-center px-12 text-black dark:text-gray-50">
     <div className="flex flex-row w-10/12 sm:w-1/4 justify-around items-center text-black dark:text-gray-50">
-      <Link
-        id="npm-packages-page"
-        href="https://www.npmjs.com/~celio1878"
-        target="_blank"
-      >
+      <NpmPackagesLink>
         <NpmIcon />
-      </Link>
-      <Link
-        id="github-page"
-        href="https://github.com/Celio1878"
-        target="_blank"
-      >
+      </NpmPackagesLink>
+
+      <GithubLink>
         <GithubIcon />
-      </Link>
-      <Link
-        id="linkedin-page"
-        href="https://www.linkedin.com/in/c%C3%A9lio-vieira-6362a284"
-        target={"_blank"}
-      >
+      </GithubLink>
+
+      <LinkedinLink>
         <LinkedInIcon />
-      </Link>
+      </LinkedinLink>
     </div>
-    <Link
-      id="vercel-page"
-      className="hover:underline"
-      href="https://vercel.com/about"
-      target={"_blank"}
-    >
-      Powered by Vercel
-    </Link>
+    <VercelLink />
   </footer>
 );
