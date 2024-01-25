fa751f7759e63139eb08b45cc98ab7f1f304b4b6
Celio <celio1878@hotmail.com>
2024-01-24 21:40:42 -0300

    style: Update dark theme

--- src/components/myPackages.tsx
+++ src/components/myPackages.tsx
@@ -34,7 +34,7 @@ interface NpmPackageProps {
 const NpmPackage: FC<NpmPackageProps> = ({ name }) => {
   return (
     <NpmPackagesLink
-      className="border border-2 border-emerald-500 p-6 rounded-md hover:bg-emerald-500 hover:text-white transition delay-75 mb-4"
+      className="border border-2 border-emerald-500 dark:border-emerald-700 p-6 rounded-md hover:bg-emerald-500 hover:text-white transition delay-75 mb-4"
       complement="package/nodejs-appbuilder"
     >
       {name}
@@ -49,7 +49,7 @@ interface PackageItemProps {
 
 const PackageItem: FC<PackageItemProps> = ({ name, description }) => {
   return (
-    <li className="w-full sm:w-2/5 flex flex-col items-center justify-center text-center border border-slate-200 py-14 px-20 rounded-md">
+    <li className="w-full sm:w-2/5 flex flex-col items-center justify-center text-center border border-slate-200 dark:border-slate-800 py-14 px-20 rounded-md">
       <NpmPackage {...{ name }} />
 
       <p className="text-center">{description}</p>
--- src/components/oldCompanies.tsx
+++ src/components/oldCompanies.tsx
@@ -3,12 +3,14 @@ import { OldCompanyLink } from "@/components/links/oldCompanyLink";
 
 export const OldCompanies: FC = () => (
   <div>
-    But I&apos;ve already worked as Senior Software Developer at
+    I&apos;ve worked as
+    <span className="font-semibold mx-1">Senior Software Developer</span> at
     <OldCompanyLink
       name="Squadra Digital"
       link="https://www.squadra.com.br/index.html"
     />
-    and FullStack Developer at
+    and <span className="font-semibold mx-1">FullStack Developer</span>
+    at
     <OldCompanyLink name="Editora Fórum" link="https://editoraforum.com.br/" />
     using many services like
   </div>
