06adfc81033df8fb4718fc9bb9558988cce6828c
Celio <celio1878@hotmail.com>
2024-04-07 15:46:11 -0300

    feat: Update email to

--- src/components/contactMe.tsx
+++ src/components/contactMe.tsx
@@ -11,7 +11,7 @@ export const ContactMe: FC<Props> = ({ dict }) => (
   <div className="absolute top-3.5 right-6">
     <Link
       className="text-black dark:text-white font-bold underline text-xl px-4 py-2 rounded-md hover:text-white hover:bg-emerald-600 transition-colors duration-300 ease-in-out"
-      href="mailto:celio1878@hotmail.com?&body=Hi Célio!"
+      href="mailto:contato@celiovieira.com?&body=Hi Célio!"
       target="_top"
     >
       {dict.contact_me}
