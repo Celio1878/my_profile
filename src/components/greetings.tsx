eaf7ca9cd463dac2990e5bd7860968dfdb5d39a3
Celio <celio1878@hotmail.com>
2024-04-08 09:48:53 -0300

    style: Responsible

--- src/components/greetings.tsx
+++ src/components/greetings.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 export const Greetings: FC<Props> = ({ dict }) => {
   return (
-    <div className="flex flex-col gap-y-4 text-4xl font-bold items-center justify-center">
+    <div className="flex flex-col gap-y-4 text-4xl font-bold items-center justify-center mb-4 mt-14 sm:mt-0">
       <ProfileImage /> {dict.greetings.title}
     </div>
   );
--- src/components/shell/asideMenu.tsx
+++ src/components/shell/asideMenu.tsx
@@ -8,14 +8,12 @@ interface Props {
 }
 
 export const AsideMenu: FC<Props> = ({ dict, lang }) => {
-  console.log(dict);
-
   return (
-    <aside className="absolute z-50 left-0 top-28 sm:top-40 sm:bg-zinc-100 w-full sm:w-1/5 lg:w-1/6 rounded-r-xl space-y-4">
+    <aside className="absolute block z-50 left-0 top-28 sm:top-48 lg:top-36 sm:bg-zinc-100 w-full sm:w-1/5 lg:w-1/6 rounded-r-xl space-y-4">
       <h2 className="hidden sm:block text-lg sm:text-xl font-medium w-full bg-zinc-200 p-3 lg:p-5 rounded-tr-xl text-zinc-400">
         Knowledge
       </h2>
-      <menu className="flex flex-row justify-center sm:flex-col space-x-8 sm:space-x-0 sm:space-y-4 mt-6 sm:mt-0 sm:pl-3 lg:pl-5 py-4 sm:py-0 sm:pb-4 border border-y-slate-600 border-x-0 sm:border-0">
+      <menu className="flex flex-row justify-center sm:flex-col space-x-4 sm:space-x-0 sm:space-y-4 mt-6 sm:mt-0 sm:pl-3 lg:pl-5 py-4 sm:py-0 sm:pb-4 border border-y-slate-600 border-x-0 sm:border-0">
         {knowledge_list.map((knowledge, index) => (
           <KnowledgeList
             key={index}
