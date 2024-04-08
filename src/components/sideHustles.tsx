652941d6112ed99470d43c28c40003acafa4f5c0
Celio <celio1878@hotmail.com>
2024-04-08 12:13:20 -0300

    fix: SideHustle languages

--- src/components/sideHustles.tsx
+++ src/components/sideHustles.tsx
@@ -2,25 +2,12 @@ import { FC } from "react";
 import { MyPackages } from "@/components/myPackages";
 
 interface Props {
-  dict: {
-    side_hustles: {
-      title: string;
-      description: string;
-      subtitle: string;
-      kind_of_packages: string;
-    };
-    packages: {
-      description1: string;
-      description2_title: string;
-      description2_source: string;
-      description2_doc: string;
-    };
-  };
+  dict: { side_hustles: { description: string } };
 }
 
 export const SideHustles: FC<Props> = ({ dict }) => (
   <div className="flex flex-col items-center justify-center gap-y-6">
     <p>{dict.side_hustles.description}.</p>
-    <MyPackages {...{ dict }} />
+    <MyPackages />
   </div>
 );
