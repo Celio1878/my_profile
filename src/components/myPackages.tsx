34fc3ec6c4c61bec9a9d6152df6cee6a1a9df315
Celio <celio1878@hotmail.com>
2024-04-09 15:12:16 -0300

    fix: packages link

--- src/components/myPackages.tsx
+++ src/components/myPackages.tsx
@@ -4,8 +4,15 @@ import { PackageItem } from "@/components/npmPackages";
 export const MyPackages: FC = () => {
   return (
     <ul className="w-full flex flex-row items-center justify-around">
-      <PackageItem name="NodeJS App Builder" />
-      <PackageItem name="CDK Factory" />
+      <PackageItem
+        {...{
+          name: "NodeJS App Builder",
+          complement: "package/nodejs-appbuilder",
+        }}
+      />
+      <PackageItem
+        {...{ name: "CDK Factory", complement: "package/cdk-factory" }}
+      />
     </ul>
   );
 };
--- src/components/npmPackages.tsx
+++ src/components/npmPackages.tsx
@@ -3,13 +3,14 @@ import { NpmPackagesLink } from "@/components/links/npmPackagesLink";
 
 interface NpmPackageProps {
   name: string;
+  complement?: string;
 }
 
-const NpmPackage: FC<NpmPackageProps> = ({ name }) => {
+const NpmPackage: FC<NpmPackageProps> = ({ name, complement }) => {
   return (
     <NpmPackagesLink
       className="border-2 border-emerald-500 dark:border-emerald-700 p-6 rounded-md hover:bg-emerald-500 hover:text-white transition delay-75 mb-4 text-sm"
-      complement="package/nodejs-appbuilder"
+      {...{ complement }}
     >
       {name}
     </NpmPackagesLink>
@@ -18,12 +19,13 @@ const NpmPackage: FC<NpmPackageProps> = ({ name }) => {
 
 interface PackageItemProps {
   name: string;
+  complement?: string;
 }
 
-export const PackageItem: FC<PackageItemProps> = ({ name }) => {
+export const PackageItem: FC<PackageItemProps> = ({ name, complement }) => {
   return (
     <li className="flex flex-col text-center rounded-md">
-      <NpmPackage {...{ name }} />
+      <NpmPackage {...{ name, complement }} />
     </li>
   );
 };
